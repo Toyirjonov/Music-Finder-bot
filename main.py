a48e9607fa1b55ed78d9a06219ecd043e2d5f270
Shoxruxmirzo <shoxruxmirzo12345@mail.ru>
2024-08-23 18:51:14 +0500

    first

---
+++ main.py
@@ -0,0 +1,56 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+import yt_dlp
+import os
+import time
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token="7448556215:AAEwtFmRU0sb1SjXuOXkA03dAtJOmgT3H6s") 
+dp = Dispatcher(bot)
+
+class FilenameCollectorPP(yt_dlp.postprocessor.common.PostProcessor):
+    def __init__(self):
+        super(FilenameCollectorPP, self).__init__(None)
+        self.filenames = []
+
+    def run(self, information):
+        self.filenames.append(information["filepath"])
+        return [], information
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply("Приветствую! Введите название ролика, и я найду его для вас.")
+
+@dp.message_handler()
+async def search(message: types.Message):
+    query = message.text
+    await message.reply('Ожидайте...')
+    
+    YDL_OPTIONS = {
+        'format': 'bestaudio/best',
+        'noplaylist': 'True',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'outtmpl': '%(title)s.%(ext)s',
+    }
+    
+    with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
+        filename_collector = FilenameCollectorPP()
+        ydl.add_post_processor(filename_collector)
+        try:
+            video = ydl.extract_info(f"ytsearch:{query}", download=True)['entries'][0]
+            filepath = filename_collector.filenames[0]
+            if filepath.endswith(".mp3"):
+                await message.reply_document(open(filepath, 'rb'))
+                await message.reply(f'Файл был отправлен!\nСпасибо за использование бота\n\n__{query}__')
+                time.sleep(5)
+                os.remove(filepath)
+        except Exception as e:
+            await message.reply(f"Произошла ошибка: {str(e)}")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
